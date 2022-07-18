132bbe45d69bb92b27d263347d4b7fc43600d89d
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-18 13:21:31 -0400

    removing ws_bootstrap

--- ws_bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get update -y
-curl https://raw.githubusercontent.com/james-cole2015/urban-sniffle/main/ws_bootstrap.sh > ws_bootstrap.sh
-chmod +x ws_bootstrap.sh
-./ws_bootstrap.sh
