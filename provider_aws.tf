2e34553dfe245e54edc02ceaa5c840c31b91278f
brooks <brooks.houck@gmail.com>
2022-07-25 21:27:03 +0000

    adding s3 remote backend

--- provider_aws.tf
+++ provider_aws.tf
@@ -1,10 +1,18 @@
 terraform {
+  backend "s3" {
+    bucket = "averyclark-tf-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "s3backend-tf"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
+
+
 }
 
 provider "aws" {
