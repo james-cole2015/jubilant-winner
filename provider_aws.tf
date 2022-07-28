7bb5a3945e7a8dc4b1a0b678739aec82665bf198
bhouck-aws <brooks.houck@gmail.com>
2022-07-28 20:42:48 +0000

    removing config line

--- provider_aws.tf
+++ provider_aws.tf
@@ -18,5 +18,5 @@ terraform {
 provider "aws" {
   region                   = var.region
   shared_credentials_files = ["~/.aws/credentials"]
-  shared_config_files  = [~/.aws/config] 
+  #shared_config_files  = ["~/.aws/config"] 
 }
